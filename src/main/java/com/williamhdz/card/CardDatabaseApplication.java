2e0dafff3ad58552a41d8ae74aee5050710d8af1
Willian <williamhernandez450@gmail.com>
2023-05-25 06:07:11 -0600

    project structure

---
+++ src/main/java/com/williamhdz/card/CardDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.williamhdz.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/williamhdz/card/CardDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamhdz.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
